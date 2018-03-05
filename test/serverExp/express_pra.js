9a403b6a312c26b8079a78a25713101ad5e3179a
Caleb Liu <b69liu@uwaterloo.ca>
2018-03-04 22:25:08 -0500

    upload files

--- test/serverExp/express_pra.js
+++ test/serverExp/express_pra.js
@@ -2,8 +2,12 @@ var express = require('express');
 var app = express();
 var path = require('path');
 var bodyParser = require('body-parser');
+var fs = require('fs');
+var multer = require('multer');
+
 
 var urlencodedParser = bodyParser.urlencoded({ extended: false }) //use querystring
+var upload = multer({dest:'/temp/'});
 
 app.use(express.static('public'));
 
@@ -26,6 +30,29 @@ app.post('/',urlencodedParser, function(req,res){
 		console.log('post');
 		if(!req.body) return res.sendStatus(400);
 		res.send('welcome ' + req.body.first_name);
+});
+
+app.post('/file_upload', upload.array('image'), function(req,res){
+		console.log('uploading file:');
+		console.log(req.files[0]);
+		var des_file = __dirname + '/' + req.files[0].originalname;
+		fs.readFile(req.files[0].path, function(err,data){
+				if(err){
+				    console.log(err);
+				}else{
+				    fs.writeFile(des_file,data,function(err){
+							if(err){
+							    console.log(err);
+							}else{
+							   // res.write('file: '+req.files[0].originalname+ ' uploaded');
+
+							}
+
+					});
+				}
+		});
+		res.end('file: '+req.files[0].originalname+ ' uploaded');
+		
 });
 
 
